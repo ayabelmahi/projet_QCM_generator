6684f960dfa7bb2b72d5c19c620980fcc43928ed
wiame Bouziane <wiame.bouziane13@gmail.com>
2026-02-16 01:40:03 +0100

    feat: setup Qcm, Question and Choice entities with repositories

--- backend/src/Entity/Question.php
+++ backend/src/Entity/Question.php
@@ -6,6 +6,7 @@ use ApiPlatform\Metadata\ApiResource;
 use App\Repository\QuestionRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
+use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: QuestionRepository::class)]
