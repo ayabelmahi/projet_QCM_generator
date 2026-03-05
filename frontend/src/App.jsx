20a6dbeaadd857789882aa29816bd24798f320c3
wiame Bouziane <wiame.bouziane13@gmail.com>
2026-03-05 01:39:44 +0100

    feat(frontend): add dashboard UI components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,20 +2,24 @@ import React from 'react'
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
 import Login from './pages/Login'
 import Register from './pages/Register'
+import Dashboard from './pages/Dashboard'
+
 
 function App() {
     return (
-        <Router>
+        
             <Routes>
                 {/* Par défaut, on arrive sur le Login */}
                 <Route path="/" element={<Navigate to="/login" replace />} />
 
                 <Route path="/login" element={<Login />} />
                 <Route path="/register" element={<Register />} />
+                <Route path="/dashboard" element={<Dashboard />} />
+
 
                 {/* On ajoutera le Dashboard ici plus tard */}
             </Routes>
-        </Router>
+        
     )
 }
 
